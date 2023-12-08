65198afde0cb8f942152edae3a13cf1008db3d3f
znacer99 <naceurez95@gmail.com>
2023-12-08 16:09:51 +0100

    2

--- math/linear_algebra/2-size_me_please.py
+++ math/linear_algebra/2-size_me_please.py
@@ -5,32 +5,10 @@ module to calculate size of a matrix
 
 def matrix_shape(matrix):
     """
-    Matrix shape function : this function calculates the length
-    of the first dimension of the Matrix and then retrieves the first element
-    to calculate the next dimension.
+    calculates size of a matrix
     """
-
-    size = []
-    if isinstance(matrix, list):
-        size.append(len(matrix))
-
-        element = matrix[0]
-        if isinstance(element, list):
-            size.extend(matrix_shape_2(element, size))
-    return size
-
-
-def matrix_shape_2(matrix, size):
-    """
-    Matrix_shape_2 is the matrix shape function that works recursively
-    with the first matrix shape function and it's the same function but
-    it's recursively.
-    """
-
-    if isinstance(matrix, list):
-        size.append(len(matrix))
-
-        element = matrix[0]
-        if isinstance(element, list):
-            size.extend(matrix_shape_2(element, size))
-    return size
+    shape = []
+    while isinstance(matrix, list):
+        shape.append(len(matrix))
+        matrix = matrix[0]
+    return shape
