a6fcf18ca88bc2377d5f1ffbaf0f04fca5f358bb
znacer99 <naceurez95@gmail.com>
2023-12-09 23:03:10 +0100

    concatenate two arrays

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Module that concatenates two arrays
+"""
+
+
+def cat_arrays(arr1, arr2):
+    """
+    Concatenates two arrays
+    """
+    result = arr1 + arr2
+    return result
