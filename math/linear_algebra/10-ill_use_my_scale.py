33e0c6602943714ee8c2ba3f88bc9f0012809c12
znacer99 <naceurez95@gmail.com>
2023-12-10 23:15:35 +0100

    shape

--- math/linear_algebra/10-ill_use_my_scale.py
+++ math/linear_algebra/10-ill_use_my_scale.py
@@ -8,11 +8,4 @@ def np_shape(matrix):
     """
     function
     """
-    shape_tuple = ( )
-    current_dim = matrix
-
-    while type(current_dim) == list:
-        shape_tuple += (len(current_dim),)
-        current_dim = current_dim[0]
-
-    return shape_tuple
+    return matrix.shape
