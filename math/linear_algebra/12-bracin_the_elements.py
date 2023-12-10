036f4355c063472967b6ed4dbc9f0082b20fec70
znacer99 <naceurez95@gmail.com>
2023-12-10 23:30:50 +0100

    add, sub, mul, div

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Module
+"""
+
+
+def np_elementwise(mat1, mat2):
+    """
+    function
+    """
+    add = mat1 + mat2
+    sub = mat1 - mat2
+    mul = mat1 * mat2
+    div = mat1 / mat2
+    return add, sub, mul, div
