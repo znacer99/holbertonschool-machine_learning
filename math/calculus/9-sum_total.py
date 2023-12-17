259db8c9c0dbd3075df526adf45d2151e5dfc1cd
znacer99 <naceurez95@gmail.com>
2023-12-17 22:36:46 +0100

    9

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -2,19 +2,12 @@
 """
 module
 """
-import sys
-
 
 def summation_i_squared(n):
     """
     function
     """
-    sys.setrecursionlimit(10**6)
 
-    if not isinstance(n, int) or n < 1:
+    if type(n) is not int or n < 1:
         return None
-
-    if n == 1:
-        return 1
-
-    return n**2 + summation_i_squared(n - 1)
+    return int((n * (n+1) * (2* n + 1)) / 6)
