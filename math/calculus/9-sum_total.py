2f76574b5470961cbdf3210a8158f2847fda5139
znacer99 <naceurez95@gmail.com>
2023-12-17 22:37:57 +0100

    9

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -3,6 +3,7 @@
 module
 """
 
+
 def summation_i_squared(n):
     """
     function
@@ -10,4 +11,4 @@ def summation_i_squared(n):
 
     if type(n) is not int or n < 1:
         return None
-    return int((n * (n+1) * (2* n + 1)) / 6)
+    return int((n * (n+1) * (2 * n + 1)) / 6)
