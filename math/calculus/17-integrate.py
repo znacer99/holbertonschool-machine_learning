a951b120141e9119f87f247b3a495b28c273a44f
znacer99 <naceurez95@gmail.com>
2023-12-17 22:55:04 +0100

    rest

---
+++ math/calculus/17-integrate.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+Module
+"""
+
+
+def poly_integral(poly, C=0):
+    """function"""
+    if type(poly) != list or len(poly) < 1:
+        return None
+    if type(C) != int and type(C) != float:
+        return None
+    if poly == [0]:
+        return [C]
+    result = all(type(s) in [int, float] for s in poly)
+    if not result:
+        return None
+    poly.insert(0, C)
+    for x in range(1, len(poly)):
+        poly[x] = poly[x] / x
+    for y in range(len(poly)):
+        if poly[y] % 1 == 0:
+            poly[y] = int(poly[y])
+    return poly
