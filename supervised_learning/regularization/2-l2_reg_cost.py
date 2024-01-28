e453291a238379cb9e13fd3f6ae79b074d4ea049
znacer99 <naceurez95@gmail.com>
2024-01-28 22:52:17 +0100

    2

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
-"""Task 2"""
+"""
+calculates the cost of a neural network with L2 regularization
+"""
+
 
 import tensorflow as tf
 
 
-def l2_reg_cost(cost):
-    """Calculates the cost of neural
-    network with L2 regularization"""
-    return (cost + tf.losses.get_regularization_losses())
+def l2_reg_cost(cost, lam):
+    """
+    cost of nn using L2 regularization
+    """
+    l2_reg_cost = tf.losses.get_regularization_losses()
+    return (cost + l2_reg_cost)
