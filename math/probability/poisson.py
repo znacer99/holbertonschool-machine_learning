a79aca9f9e9885d20de65ba132356e2c479893cf
znacer99 <naceurez95@gmail.com>
2024-06-15 06:52:50 +0100

    Poisson

---
+++ math/probability/poisson.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+"""
+Creates a class for Poisson
+"""
+
+class Poisson:
+    """
+    Poisson class for distribution
+    """
+
+    def __init__(self, data=None, lambtha=1.):
+        self.lambtha = float(lambtha)
+        if self.lambtha <= 0:
+            raise ValueError("lambtha must be a positive value")
+        if data is not None:
+            if type(data) is not list:
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = float(sum(data)) / len(data)
