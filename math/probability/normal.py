2e6844b08cbad8fa88bb173f5cfc46806dfbc53c
znacer99 <naceurez95@gmail.com>
2024-07-24 16:59:12 +0100

    try

--- math/probability/normal.py
+++ math/probability/normal.py
@@ -57,3 +57,16 @@ class Normal:
         stddev = self.stddev
         x = (z * stddev) + mean
         return x
+
+    def pdf(self, x):
+        """
+        Calculates the value of the PDF for a given x-value
+        """
+        mean = self.mean
+        stddev = self.stddev
+        e = 2.7182818285
+        pi = 3.1415926536
+        power = -0.5 * (self.z_score(x) ** 2)
+        coefficient = 1 / (stddev * ((2 * pi) ** (1 / 2)))
+        pdf = coefficient * (e ** power)
+        return pdf
