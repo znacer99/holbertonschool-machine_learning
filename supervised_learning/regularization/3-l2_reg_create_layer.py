3569399da33a9bcd94798a2d89ff02a6acfe8a4e
znacer99 <naceurez95@gmail.com>
2024-10-23 23:51:11 +0100

    3

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Module used to
+Create a Layer with L2 Regularization.
 """
 
 import tensorflow as tf
@@ -8,30 +8,29 @@ import tensorflow as tf
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
     """
-    creates a tensorflow layer that includes L2 regularization
-    Args:
-        - prev is a tensor containing the output of the previous layer
-        - n is the number of nodes the new layer should contain
-        - activation is the activation funct that should be used on the layer
-        - lambtha is the L2 regularization paramet
+    Creates a neural network layer with L2 regularization.
+
+    Parameters:
+    prev (tensor): tensor containing the output of the previous layer.
+    n (int): number of nodes the new layer should contain.
+    activation (function): activation function to use on the layer.
+    lambtha (float): L2 regularization parameter.
+
     Returns:
-        the output of the new layer
+    tensor: the output of the new layer.
     """
+    # Weight initialization: He et. al
+    init_weights = tf.keras.initializers.VarianceScaling(scale=2.0,
+                                                         mode=("fan_avg"))
+    # Regularization for this layer's loss: L2 regularizer
+    l2_regularizer = tf.keras.regularizers.L2(lambtha)
 
-    # tf.contrib.layers.l2_regularizer
-    # Returns a funct that can be used to apply L2 regularization to weights.
+    # Create Dense layer with L2 regularization
+    layer = tf.keras.layers.Dense(
+        units=n,
+        activation=activation,
+        kernel_initializer=init_weights,
+        kernel_regularizer=l2_regularizer
+    )
 
-    λ = lambtha
-
-    raw_layer = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
-    new_regularize = tf.contrib.layers.l2_regularizer(scale=λ, scope=None)
-    # (1)
-    # init = tf.global_variables_initializer()
-
-    output_tensor = tf.layers.Dense(units=n,
-                                    activation=activation,
-                                    kernel_initializer=raw_layer,
-                                    kernel_regularizer=new_regularize,
-                                    name="layer")
-
-    return(output_tensor(prev))
+    return layer(prev)
