067942740baebb4a7ba3344bffd3d63c118d5cc7
znacer99 <naceurez95@gmail.com>
2024-10-23 23:46:20 +0100

    2

--- supervised_learning/regularization/2-l2_reg_cost.py
+++ supervised_learning/regularization/2-l2_reg_cost.py
@@ -12,5 +12,13 @@ def l2_reg_cost(cost, lam):
     """
     cost of nn using L2 regularization
     """
-    l2_reg_cost = tf.losses.get_regularization_losses()
-    return (cost + l2_reg_cost)
+     # Get all trainable variables (weights)
+    weights = tf.trainable_variables()
+
+    # Compute L2 regularization term: sum of squared weights multiplied by lambda
+    l2_loss = tf.add_n([tf.nn.l2_loss(w) for w in weights])
+
+    # Add L2 regularization to the original cost
+    total_cost = cost + lam * l2_loss
+
+    return total_cost
